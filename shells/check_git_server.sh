c4ade16f1cc0c8aaafe4cadbb887bc93f4bc1a97
Hubery He <hyc171819@gmail.com>
2015-12-24 18:24:32 +0800

    get public from dicovi.com, record the ip address at the same time.

--- shells/check_git_server.sh
+++ shells/check_git_server.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-parse_json(){
-
-echo $1 | sed 's/.*'$2':\ \([^,}]*\).*/\1/'
-
-}
-
 nslookupIP=$(nslookup hyc171819.vicp.cc 118.118.118.118 | grep Address | sed -n '2p' | awk '{print $2}')
-publicIP=$(curl 178.62.234.75:16000/api/v1.0 | grep remote | awk '{print $2}' | sed 's/\"//g')
+# publicIP=$(curl 178.62.234.75:16000/api/v1.0 | grep remote | awk '{print $2}' | sed 's/\"//g')
+publicIP=$(curl -s http://www.dicovi.com/shows/git?ns=$nslookupIP | sed 's/.*"remote":"\([^",}]*\).*/\1/')
 
 echo "nslookupIP = $nslookupIP"
 echo "publicIP = $publicIP"
