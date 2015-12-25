f00508d15a7d0ebb5e6bd0a8b7940c780435ea3a
Hubery He <hyc171819@gmail.com>
2015-12-25 17:57:14 +0800

    dev

--- shells/check_git_server.sh
+++ shells/check_git_server.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 Resettem=$(tput sgr0)
-nslookupIP=$(nslookup hyc171819.vicp.cc 118.118.118.118 | grep Address | sed -n '2p' | awk '{print $2}')
+serverDomainName="hyc171819.vicp.cc"
+echo "Checking $serverDomainName..."
+
+nslookupIP=$(nslookup $serverDomainName 118.118.118.118 | grep Address | sed -n '2p' | awk '{print $2}')
+echo "Got nslookup IP: $nslookupIP"
 # publicIP=$(curl 178.62.234.75:16000/api/v1.0 | grep remote | awk '{print $2}' | sed 's/\"//g')
 publicIP=$(curl -s http://www.dicovi.com/shows/git?ns=$nslookupIP | sed 's/.*"remote":"\([^",}]*\).*/\1/')
+echo "Got public IP: $publicIP"
+printf "\n"
 
-echo "nslookupIP = $nslookupIP"
-echo "publicIP = $publicIP"
-
-if [[ $nslookupIP -eq $publicIP ]]; then
+if [[ $nslookupIP == $publicIP ]]; then
 	#statements
-	echo -e '\E[33m' "hyc171819.vicp.cc is all right." $Resettem "Check completed!"
+	echo -e '\E[33m' "$serverDomainName is all right." $Resettem
 else
-	echo -e '\E[33m' "hyc171819.vicp.cc is error. Right IP for git server is " $publicIP $Resettem "Check completed!"
+	echo -e '\E[31m' "$serverDomainName is error. Right IP for GIT server is" $publicIP $Resettem
 fi
+
+echo "Check completed!"
