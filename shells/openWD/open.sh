80600f3a1e7dc758057a80b3c3b5affccb417bea
Hubery He <hyc171819@gmail.com>
2016-12-06 20:11:53 +0800

    161206 - nightly

--- shells/openWD/open.sh
+++ shells/openWD/open.sh
@@ -74,13 +74,12 @@ function delete ()
 
 function usage ()
 {
-	echo "Usage: bash $0 <open/up/down/add/delete> [dirname]"
+	echo "Usage: bash $0 <up/down/add/delete> [dirname]"
 	echo "Help to sync git repo every day and night."
-	echo "	open				open all work directorys"
-	echo "	up				automatically push all git repo after dayly work"
-	echo "	down				automatically pull all git repo before dayly work"
-	echo "	add [dirname]			add a git repo, current directory in default"
-	echo "	delete [dirname]		delete a git repo, current directory in default"
+	echo "	bash $0 up					automatically push all git repo after dayly work"
+	echo "	bash $0 down				automatically pull all git repo before dayly work"
+	echo "	bash $0 add [dirname]			add a git repo, "
+	echo "	bash $0 delete [dirname]			delete a git repo"
 }
 
 
@@ -104,10 +103,6 @@ function blue() {
     echo -e "\033[34m$1\033[0m"
 }
 
-if [[ $# -lt 1 ]]; then
-	usage
-	exit 1
-fi
 
 scriptPath=$(dirname $0)
 scriptPath=${scriptPath/\./$(pwd)}
@@ -121,11 +116,8 @@ commit="`date +%y%m%d` - nightly"
 # sleep 1;
 # xdotool type --delay 1 --clearmodifiers "cd";
 # xdotool key Return;
-###### http://stackoverflow.com/questions/1188959/open-a-new-tab-in-gnome-terminal-using-command-line
-###### http://blog.csdn.net/swust_long/article/details/7285147
-###### http://blog.csdn.net/namelcx/article/details/49453911
 
-if [[ $1 == 'open' ]]; then
+if [[ $# -eq 0 ]]; then
 	openCMD=''
 	while read line; do
 		if [[ ${line:0:1} == '#' ]]; then
@@ -136,16 +128,14 @@ if [[ $1 == 'open' ]]; then
 		dirFullName=$(pwd)
 		cd -P - >/dev/null
 		yellow "open -> ${dirFullName}"
-		if [[ ! -d $1 ]]; then
-			red 'dir not exist.'
-			continue
-		fi
+		dirFullNameParse=${dirFullName//\//\\/}
 		openCMD=${openCMD}"--tab -e 'bash -c \"cd ${dirFullName}; exec bash\"' "
 	done < $iniFile
 	bash -c "gnome-terminal $openCMD--maximize"
-	xdotool key ctrl+alt+s
+	exit 0
+fi
 
-elif [[ $1 == 'up' ]]; then
+if [[ $1 == 'up' ]]; then
 	cat $iniFile | while read line; do
 		if [[ ${line:0:1} == '#' ]]; then
 			continue
@@ -153,6 +143,7 @@ elif [[ $1 == 'up' ]]; then
 		yellow "up -> ${line}"
 		push $line
 	done
+
 elif [[ $1 == 'down' ]]; then
 	cat $iniFile | while read line; do
 		if [[ ${line:0:1} == '#' ]]; then
